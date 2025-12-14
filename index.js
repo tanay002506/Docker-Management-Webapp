68fd93e39caf0626431a442fac3c2acae10dad2c
Tanay Kandpal <tanay@kali.kandpaltanay00@gmail.com>
2025-12-14 12:31:51 +0530

    initial docker management project commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname+'/index.html');
+})
+
+app.listen(3000,(req,res)=>{
+    console.log('server started at port 3000');
+});
+
