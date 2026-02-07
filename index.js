5df3ad1681656e52255e502bcd1cf6a375a69791
Tanay Kandpal <tanay@MSI.localdomain>
2026-02-07 12:47:26 +0530

    added functionality to show running containers and all created containers

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+const { exec } = require("child_process");
+const { stderr } = require("process");
+
 
 const app = express();
 const PORT = 3000;
@@ -25,6 +28,81 @@ app.post("/launch-container", (req, res) => {
         message: `Container '${name}' with image '${osImage}' received`
     });
 });
+
+app.get("/show_running_images", (req,res)=>{
+    exec(
+        'docker ps --format "{{.Names}}|{{.Image}}|{{.Status}}"',
+        (error, stdout, stderr)=>{
+
+            if(error){
+                return res.status(500).json({
+                    sucess:false,
+                    error:error.message
+                });
+            }
+
+            if(!stdout.trim()) {
+                return res.json({
+                    sucess:true,
+                    runnning:[]
+                });
+            }
+
+            const containers =stdout
+                .trim()
+                .split("\n")
+                .map(line =>{
+
+                    const[name, image, status] = line.split("|");
+                    return{ name, image, status };
+                })
+            
+            res.json({
+                success:true,
+                running: containers
+            })
+        }
+
+    )
+})
+
+app.get("/show_all_containers",(req,res)=>{
+    exec(
+        'docker ps -a --format "{{.Names}}|{{.Image}}|{{.Status}}"',
+        (error,stdout, stderr)=>{
+
+            if(error){
+                return res.status(500).json({
+                    success:false,
+                    error:error.message
+                });
+            }
+
+            if(!stdout.trim()) {
+                return res.json({
+                    sucess:true,
+                    runnning:[]
+                });
+            }
+
+            const containers_all =stdout
+                .trim()
+                .split("\n")
+                .map(line =>{
+
+                    const[name, image, status] = line.split("|");
+                    return{ name, image, status };
+                })
+            
+            res.json({
+                success:true,
+                running: containers_all
+            })
+
+
+        }
+    )
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
